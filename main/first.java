043ba1729a16c9ae58c9431b490a484953fd574d
aerabit <aerabit@proton.me>
2026-02-06 11:52:34 -0800

    Update first.java

--- main/first.java
+++ main/first.java
@@ -3,5 +3,11 @@ public class first {
 
     public static void main(String[] args) {
         // test comment
+        Integer original = new Integer(8);
+
+        Integer first = new Integer(original.intValue() * 2);
+
+        Integer second = new Integer(original.intValue() + 2);
+
     }
 }
